993d5da2654fbf42887d297d08e5e33693d283e6
Keiko Ueda <keiko77mel@gmail.com>
2021-01-26 18:59:26 +1100

    updated invoice001.py

--- invoice001.py
+++ invoice001.py
@@ -1,2 +1,15 @@
+# coding: utf-8
+import os
 import openpyxl
 
+wb = openpyxl.Workbook()
+ws = wb.active
+ws.title = 'invoice'
+ws['A1'] = 'T-shirt'
+ws.cell(row=1, column=2, value=10)
+
+path_book = 'excel'
+if not os.path.isdir(path_book):
+    os.makedirs(path_book)
+
+wb.save(path_book + '/text.xlsx')
